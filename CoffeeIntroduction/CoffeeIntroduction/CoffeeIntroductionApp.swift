44c15103d32e5359801b410d23e79aed311ec0ac
Tori <ttang1135@naver.com>
2023-04-26 01:31:06 +0900

    init Project

---
+++ CoffeeIntroduction/CoffeeIntroduction/CoffeeIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeIntroductionApp.swift
+//  CoffeeIntroduction
+//
+//  Created by 방유빈 on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
